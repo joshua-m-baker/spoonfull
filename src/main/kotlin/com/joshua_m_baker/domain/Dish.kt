13aad6763ab572492307fd7f54bba54f7c63eb96
Josh <joshuabaker97@gmail.com>
2025-02-21 20:52:17 -0600

    fix serialization, add test

--- src/main/kotlin/com/joshua_m_baker/domain/Dish.kt
+++ src/main/kotlin/com/joshua_m_baker/domain/Dish.kt
@@ -1,5 +1,8 @@
 package com.joshua_m_baker.domain
 
+import io.micronaut.serde.annotation.Serdeable
+
+@Serdeable
 data class Dish(
     val dishName: String,
     val dishDescription: String? = null,
--- src/test/kotlin/com/joshua_m_baker/SpoonfullTest.kt
+++ src/test/kotlin/com/joshua_m_baker/SpoonfullTest.kt
@@ -2,11 +2,25 @@ package com.joshua_m_baker
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import jakarta.inject.Inject
 
 @MicronautTest
-class SpoonfullTest(private val application: EmbeddedApplication<*>): StringSpec({
+class SpoonfullTest(
+    private val application: EmbeddedApplication<*>,
+    @Client("/") private val client: HttpClient
+): StringSpec({
+
 
     "test the server is running" {
         assert(application.isRunning)
+    }
+
+    "gets response" {
+        val response = client.toBlocking().exchange<String>("/experiences")
+        response.code() shouldBe HttpStatus.OK.code
     }
 })
