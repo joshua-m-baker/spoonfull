bbdc143ff0f6c1876817414f87179b82880d0dbf
Josh <joshuabaker97@gmail.com>
2025-03-09 22:54:12 -0500

    make id readonly

--- src/main/kotlin/com/joshua_m_baker/domain/Restaurant.kt
+++ src/main/kotlin/com/joshua_m_baker/domain/Restaurant.kt
@@ -1,10 +1,11 @@
 package com.joshua_m_baker.domain
 
 import io.micronaut.serde.annotation.Serdeable
+import io.swagger.v3.oas.annotations.media.Schema
 import java.util.*
 
 @Serdeable
 data class Restaurant(
-    val id: UUID = UUID.randomUUID(),
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY) val id: UUID = UUID.randomUUID(),
     val name: String,
 )
